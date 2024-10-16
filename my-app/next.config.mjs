45155cc232c11e333425f8c12fcf0e520aa3d814
s160saito <s160saito@gmail.com>
2024-10-16 18:48:07 +0900

    no message

--- my-app/next.config.mjs
+++ my-app/next.config.mjs
@@ -1,6 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-    output: 'standalone'
-  }
-  
-  module.exports = nextConfig
+    output: 'standalone',
+};
+
+// ESモジュール形式でエクスポート
+export default nextConfig;
